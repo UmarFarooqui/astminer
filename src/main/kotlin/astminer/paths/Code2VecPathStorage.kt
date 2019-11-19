1f56817159cf2bff2d196112cc7dd24ced8156c7
Umar Farooqui <umar.farooqui@rockwellcollins.com>
2019-11-18 21:37:05 -0500

    Printing the path context on the console rather than writing to file, this is done so that we can use this output and return from a python function in the colab notebook

--- src/main/kotlin/astminer/paths/Code2VecPathStorage.kt
+++ src/main/kotlin/astminer/paths/Code2VecPathStorage.kt
@@ -21,4 +21,20 @@ class Code2VecPathStorage(outputFolderPath: String) : CountingPathStorage<String
         writeLinesToFile(lines, file)
     }
 
+    override fun getPathContexts(file: File, tokensLimit: Long, pathsLimit: Long): MutableList<String> {
+        val lines = mutableListOf<String>()
+        labeledPathContextIdsList.forEach { labeledPathContextIds ->
+            val pathContextIdsString = labeledPathContextIds.pathContexts.filter {
+                tokensMap.getIdRank(it.startTokenId) <= tokensLimit &&
+                        tokensMap.getIdRank(it.endTokenId) <= tokensLimit &&
+                        pathsMap.getIdRank(it.pathId) <= pathsLimit
+            }.joinToString(separator = " ") { pathContextId ->
+                "${pathContextId.startTokenId},${pathContextId.pathId},${pathContextId.endTokenId}"
+            }
+            lines.add("${labeledPathContextIds.label} $pathContextIdsString")
+        }
+
+        return lines
+    }
+
 }
